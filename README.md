4f55fc1a30586a6cc1d856a070a991cbb3f09fd1
wisaTong <wisa.powthongchin@gmail.com>
2018-02-08 11:17:48 +0700

    Done

--- README.md
+++ README.md
@@ -16,13 +16,14 @@ See [Collections-practice](https://skeoop.github.io/week4/Collections-practice) 
 Answer the questions in the exercises here. Push your answer and code to Github Classroom.
 
 * When you print elements in the set, are they in same order as you added them?
-
+	No, it's not in the same order.
 * what is the command to remove all elements from a `set`?
-
+	clear();
 * When you add elements in a different order and print them, did the order change?
-
+	the order changed.
 * Can you add a duplicate "cat" and "dog" to set?
-
+	No, you can't.
 * What is the result when you use TreeSet instead of HashSet?
-
+	the results are printed in alphabetical order.
 * Could we make `setExplorer` work with either Set or List?  How?
+	set paramater to Collection
--- src/SetPractice.java
+++ src/SetPractice.java
@@ -9,29 +9,39 @@ public class SetPractice {
 	 * @parameter set is the Set to add items to
 	 * @parameter values zero or more Strings to add to set
 	 */
-	public static void addAll(Set<String> set, String ...values) {
+	public static void addAll(Collection<String> set, String ...values) {
 		// add all the values to the set. 
 		// print each one as you add it
+		for (String s : values) {
+			set.add(s);
+			System.out.print(s + " ");
+		}
+		System.out.println();
 	}
 	
 	/**
 	 * Print all the elements in a set, using an Iterator.
 	 */
-	public static void print(Set<String> set) {
+	public static void print(Collection<String> set) {
 		// create an Iterator using set.iterator()
 		// then use the iterator to print everything in the set
+		Iterator iter = set.iterator();
+		while(iter.hasNext()) {
+			System.out.print(iter.next() + " ");
+		}
+		System.out.println();
 	}
 	
 	/**
 	 * Explore behavior of a Set.
 	 */
-	public static void setExplorer( ) {
-		// 1. Create a HashSet and add strings: dog, ant, bird, elephant, cat
-		
+	public static void setExplorer(Set<String> animal) {
+		// 1. Create a HashSet and add strings: dog, ant, bird, elephant, cat 
+		addAll(animal, "dog", "ant", "bird", "elephant", "cat");
 		
 		// 2. Print elements in the set using an Iterator over the Set.
 		// You're going to need to do this again, so write a method print(Set set) to do it.
-		
+		print(animal);
 		
 		// 3. are the elements printed in same order than you added them? 
 		// What does this tell you about Sets?
@@ -39,24 +49,26 @@ public class SetPractice {
 		
 		// 4. Remove all the elements from the set, using a method that 
 		// defined in Set (and Collection). What is the method to use?
-		
+		animal.clear();
 		
 		// 5. Add same elements as exercise 1, but in a different order,
 		// such as: "cat", "elephant", "bird", "ant", "dog"
-		
+		addAll(animal, "cat", "elephant", "bird", "ant", "dog");
 		
 		// 6. Print the elements in the set.  Is the order same as before?
-		
+		print(animal);
 		
 		// 7. Add duplicate elements to the set.  Add another "cat" and "dog".
 		// To ensure that these are *distinct* objects use 'set.add(new String("cat"))'
 		// and new String("dog").
 		System.out.println("Add duplicate items to the set ");
+		animal.add(new String("cat"));
 		
 		
 		// 8. Print the elements in set.  Are there any duplicates?
 		// What property of Set does this demonstrate?
 		System.out.println("Set contains: ");
+		print(animal);
 		
 		
 		// 9. Repeat the exercises using a TreeSet instead of HashSet.
@@ -65,10 +77,18 @@ public class SetPractice {
 		// Hint: Use Polymorphism.  Make the Set object be a *parameter* to the
 		// method so you can easily use a different set just by changing the parameter!
 		
-		// 10. Repeat again using TreeSet with a Comparator.
+		// 10. Repeat again using TreeSet with a Com parator.
 	}
 
 	public static void main(String[] args) {
-		setExplorer();
+		setExplorer(new HashSet<String>());
+		
+		System.out.println("\nTreeSet with comparator\n");
+		
+		setExplorer(new TreeSet<String>(new Comparator<String>() {
+			public int compare(String a, String b) {
+				return a.length() - b.length();
+			}
+		}));
 	}
 }
